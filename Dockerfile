b97fd7128075d447d0f1c20b48a285635bde85b1
Mirko <mirko.kaempf@gmail.com>
2017-07-31 12:49:57 +0200

    Finished image for Blog post

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ MAINTAINER Mirko Kämpf "mirko@cloudera.com"
 
 # update packages and install maven
 RUN apt-get update && \
-    apt-get install -y -q graphviz gnuplot vim wget curl maven && \
+    apt-get install -y -q graphviz gnuplot vim wget curl maven mc && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -1,13 +1,25 @@
 docker build -t kamir/cdsw-base-with-maven-graphviz-gnuplot . 
 
+
+
 export T=$(date +%I_%M_%S)
-echo $T
+echo "current time is: ".$T
 
-#docker create \
-#    --name custom_cdsw_$T \
-#    kamir/cdsw-custom-2
 
-docker run \
-    --name custom_cdsw_$T \
-    kamir/cdsw-custom-2
+
+docker image ls
+
+docker run -it -d --name container_$T kamir/cdsw-base-with-maven-graphviz-gnuplot
+
+docker container ls
+
+read -p "Container-ID to connect to : " C_ID
+
+echo $C_ID 
+
+docker exec -i -t $C_ID /bin/bash
+
+docker container stop $C_ID
+
+
 
