d9137869e5a5733901ad411c52bfab5348c536bd
Mirko Kämpf <mirko.kaempf@cloudera.com>
2018-09-19 22:59:48 +0200

    Update buildAndDeploy.sh

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -1,13 +1,14 @@
 export L=cdsw-caps-cuda
+export DOCKER_USER=kamir
 
-docker build -t kamir/$L . 
+docker build -t $DOCKER_USER/$L . 
 
 export T=$(date +%I_%M_%S)
 echo "current time is: ".$T
 
 docker image ls
 
-docker run -it -d --name container_$T kamir/$L
+docker run -it -d --name container_$T $DOCKER_USER/$L
 
 docker container ls
 
@@ -18,6 +19,8 @@ echo $C_ID
 docker exec -i -t $C_ID /bin/bash
 
 docker container stop $C_ID
+
+docker push $DOCKER_USER/$L
 
 
 
